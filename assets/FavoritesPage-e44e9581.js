3deb6de995b4ed07ecd4c71dfde9e3626ebc1c60
horodylova <108434503+horodylova@users.noreply.github.com>
2024-04-19 23:01:30 +0000

    Deploying to gh-pages from @ horodylova/truck-camper@1e97f2d1d9c895bb6d7f81447d69a6826c343e9d 🚀

---
+++ assets/FavoritesPage-e44e9581.js
@@ -0,0 +1,13 @@
+import{u as o,j as t}from"./index-55473030.js";const e=o.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`,i=o.h1`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`,a=o.p`
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+`,s=()=>t.jsx(e,{children:t.jsxs("div",{children:[t.jsx(i,{children:"No Favorites Yet"}),t.jsx(a,{children:"It looks like you haven't added any favorites yet. Start exploring and add some items to your favorites list!"})]})});export{s as default};
---
+++ assets/HomePage-fca25d58.js
@@ -0,0 +1,22 @@
+import{u as o,j as e}from"./index-55473030.js";const n=o.div`
+  background-color: #fff; 
+`,a=o.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+ 
+`,i=o.h1`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`,r=o.p`
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+`,s=o.ul`
+  list-style-type: disc;
+  margin-left: 20px;
+`,t=o.li`
+  font-size: 16px;
+  line-height: 1.6;
+`,u=()=>e.jsx(n,{children:e.jsxs(a,{children:[e.jsx(i,{children:"Welcome to Our Camper Rental Company!"}),e.jsx(r,{children:"Embark on unforgettable adventures with our luxurious campers available for rent. Whether you dream of exploring natural wonders afar or traveling to picturesque destinations in your region, we'll make your journey comfortable and memorable."}),e.jsx(r,{children:"Our campers are not just transportation; they are your personal home on wheels, offering:"}),e.jsxs(s,{children:[e.jsx(t,{children:"Comfort and convenience: Discover space and coziness in our modern campers equipped with all the essentials for a comfortable getaway."}),e.jsx(t,{children:"Freedom of movement: Travel wherever your imagination takes you and stop wherever you desire, maintaining the freedom of your schedule."}),e.jsx(t,{children:"Excellent quality and service: Our campers are always maintained in top condition, and our teams are happy to assist you with everything you need."})]}),e.jsx(r,{children:"Start your journey with us today and give yourself the opportunity to savor every moment of your adventure!"})]})});export{u as default};
