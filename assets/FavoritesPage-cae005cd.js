d7e58f9cd9e9c97abc4b52d0a6ce3dd6df8739a9
horodylova <108434503+horodylova@users.noreply.github.com>
2024-04-20 17:09:21 +0000

    Deploying to gh-pages from @ horodylova/truck-camper@9662be97092b506bd0884f5bd24c3e3897892874 🚀

---
+++ assets/FavoritesPage-cae005cd.js
@@ -0,0 +1,9 @@
+import{u as e,j as t}from"./index-79e2f1a5.js";const o=e.h2`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`,s=e.p`
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+`,i=()=>t.jsx("div",{className:"background",children:t.jsxs("div",{className:"container",children:[t.jsx(o,{children:"No Favorites Yet"}),t.jsx(s,{children:"It looks like you haven't added any favorites yet. Start exploring and add some items to your favorites list!"})]})});export{i as default};
---
+++ assets/HomePage-20f6d666.js
@@ -0,0 +1,41 @@
+import{u as r,j as e,L as n}from"./index-79e2f1a5.js";const a=r.div`
+  color: var(--white-color); 
+  margin: 0 auto;
+  padding: 20px;
+`,i=r.h1`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  font-family: 'Inter';
+  src: url('/fonts/Inter-Medium.ttf') format('truetype');
+`,t=r.p`
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+  font-family: 'Inter';
+  src: url('/fonts/Inter-Medium.ttf') format('truetype');
+`,s=r.ul`
+  list-style-type: disc;
+  margin-left: 20px;
+  font-family: 'Inter';
+  src: url('/fonts/Inter-Medium.ttf') format('truetype');
+`,o=r.li`
+  font-size: 16px;
+  line-height: 1.6;
+  font-family: 'Inter';
+  src: url('/fonts/Inter-Medium.ttf') format('truetype');
+`,l=r.button`
+  border: none;
+  background-color: rgba(255, 255, 255, 0.5); 
+  color: #fff;
+  font-size: 16px;
+  font-family: 'Inter', sans-serif;
+  padding: 10px 20px; 
+  border-radius: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease; 
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2); 
+  }
+`,c=()=>e.jsx(a,{className:"background",children:e.jsxs("div",{className:"container",children:[e.jsx(i,{children:"Welcome to Our Camper Rental Company"}),e.jsx(t,{children:"Embark on unforgettable adventures with our luxurious campers available for rent. Whether you dream of exploring natural wonders afar or traveling to picturesque destinations in your region, we'll make your journey comfortable and memorable."}),e.jsx(t,{children:"Our campers are not just transportation; they are your personal home on wheels, offering:"}),e.jsxs(s,{children:[e.jsx(o,{children:"Comfort and convenience: Discover space and coziness in our modern campers equipped with all the essentials for a comfortable getaway."}),e.jsx(o,{children:"Freedom of movement: Travel wherever your imagination takes you and stop wherever you desire, maintaining the freedom of your schedule."}),e.jsx(o,{children:"Excellent quality and service: Our campers are always maintained in top condition, and our teams are happy to assist you with everything you need."})]}),e.jsx(t,{children:"Start your journey with us today and give yourself the opportunity to savor every moment of your adventure!"}),e.jsx(n,{to:"/catalog",children:e.jsx(l,{children:"Explore Catalog"})})]})});export{c as default};
