1879db2dce24a63671a41e737e395e449af75c66
svitlana-horodylova <horodylova.sv@gmail.com>
2024-04-23 15:01:57 +0100

    Added list Vans

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -9,13 +9,23 @@ import {
   EquipmentItem,
   EquipmentItemSingle,
   EquipmentText,
-  VehicleType,
+  VehicleTypeTitle,
+  VehicleList,
   RightColumn,
   SearchButton,
 } from './CatalogPage.styled';
 import Location from '../../components/Location/Location';
 import { ReactComponent as TV } from '../../Icons/Big icons/TV.svg';
 import { ReactComponent as Gear } from '../../Icons/Big icons/Gear.svg';
+import { ReactComponent as Wind} from '../../Icons/Big icons/wind.svg';
+import { ReactComponent as Shower } from '../../Icons/Big icons/Shower.svg';
+import { ReactComponent as Food } from '../../Icons/Big icons/Food.svg';
+
+import {ReactComponent as Van} from '../../Icons/Big icons/Van.svg';
+import {ReactComponent as Alcove} from '../../Icons/Big icons/alcove.svg';
+import {ReactComponent as Integrated} from '../../Icons/Big icons/integrated.svg';
+
+
 
 
 
@@ -35,35 +45,62 @@ const CatalogPage = () => {
         <VehicleEquipment>Vehicle Equipment</VehicleEquipment>
         <EquipmentList>
           <EquipmentItem>
-            <EquipmentItemSingle>
-            <TV width="32" height="32"/>
-              <EquipmentText>TV</EquipmentText>
+          
+            <EquipmentItemSingle style={{ paddingLeft: "40px", paddingRight: "40px" }}>
+            <Wind width="32" height="32"/>
+            <EquipmentText>AC</EquipmentText>
             </EquipmentItemSingle>
           </EquipmentItem>
           <EquipmentItem>
-            <EquipmentItemSingle>
+            <EquipmentItemSingle style={{ paddingLeft: "16px", paddingRight: "16px" }}>
             <Gear width="32" height="32" />
             <EquipmentText>Automatic</EquipmentText>
             </EquipmentItemSingle>
           </EquipmentItem>
           <EquipmentItem>
-            <EquipmentItemSingle></EquipmentItemSingle>
+          <EquipmentItemSingle style={{ paddingLeft: "27px", paddingRight: "27px" }}>
+            <Food width="32" height="32"/>
+            <EquipmentText>Kitchen</EquipmentText>
+            </EquipmentItemSingle>
           </EquipmentItem>
           <EquipmentItem>
-            <EquipmentItemSingle></EquipmentItemSingle>
+            <EquipmentItemSingle style={{ paddingLeft: "40px", paddingRight: "40px" }}>
+            <TV width="32" height="32"/>
+              <EquipmentText>TV</EquipmentText>
+            </EquipmentItemSingle>
           </EquipmentItem>
           <EquipmentItem>
-            <EquipmentItemSingle></EquipmentItemSingle>
+          <EquipmentItemSingle style={{ paddingLeft: "12px", paddingRight: "12px" }}>
+            <Shower width="32" height="32"/>
+            <EquipmentText>Shower/WC</EquipmentText>
+            </EquipmentItemSingle>
           </EquipmentItem>
         </EquipmentList>
-        <VehicleType>
-          Vehicle Type
-          <ul>
-            <li>Van</li>
-            <li>Fully Integrated</li>
-            <li>Alcove</li>
-          </ul>
-        </VehicleType>
+        <VehicleTypeTitle>Vehicle Type</VehicleTypeTitle>
+            <VehicleList>
+            <EquipmentItem>
+          
+            <EquipmentItemSingle style={{ paddingLeft: "30px", paddingRight: "30px" }}>
+            <Van width="40" height="28"/>
+            <EquipmentText>Van</EquipmentText>
+            </EquipmentItemSingle>
+          </EquipmentItem>
+          <EquipmentItem>
+          
+          <EquipmentItemSingle style={{ paddingLeft: "24px", paddingRight: "24px" }}>
+          <Integrated width="40" height="28"/>
+          <EquipmentText>Fully<br/>Integrated</EquipmentText>
+          </EquipmentItemSingle>
+        </EquipmentItem>
+        <EquipmentItem>
+          
+          <EquipmentItemSingle style={{ paddingLeft: "30px", paddingRight: "30px" }}>
+          <Alcove width="40" height="28"/>
+          <EquipmentText>Alcove</EquipmentText>
+          </EquipmentItemSingle>
+        </EquipmentItem>
+          </VehicleList>
+
         <SearchButton>Search</SearchButton>
       </LeftColumn>
       <RightColumn>
--- src/pages/CatalogPage/CatalogPage.styled.jsx
+++ src/pages/CatalogPage/CatalogPage.styled.jsx
@@ -65,12 +65,14 @@ width: 360px;
 align-items: flex-start;
 gap: 8px;
 flex-wrap: wrap;
+margin-bottom:32px;
 `
 
 export const EquipmentItem = styled.li`
 display: inline-block;
 height: 95px;
-padding: 17px;
+padding-top: 17px;
+padding-bottom: 17px;
 align-items: center;
 gap: 8px;
 
@@ -80,7 +82,7 @@ border: 1px solid rgba(16, 24, 40, 0.20);
 mix-blend-mode: multiply;
 
 &:hover{
-  border: 1px solid #E44848;
+  border: 1px solid var(--red-color);
 }
 
 `;
@@ -96,11 +98,34 @@ gap: 8px;
 export const EquipmentIcon = styled.img`
 `
 export const EquipmentText = styled.p`
+color: var(--primary-txt-color);
+
+text-align: center;
+font-family: Inter;
+font-size: 16px;
+font-style: normal;
+font-weight: var(--medium);
+line-height:1.25;
 `
 
-export const VehicleType = styled.div`
-  margin-bottom: 20px;
+export const VehicleTypeTitle = styled.div`
+  margin-bottom: 24px;
+  color: var(--primary-txt-color);
+
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: var(--medium);
+  line-height: 1.2;
 `;
+
+export const VehicleList = styled.ul`
+border-top: 1px solid rgba(16, 24, 40, 0.10);
+padding-top:24px;
+gap: 8px;
+`;
+export const VehicleType = styled.li`
+`
 
 export const RightColumn = styled.div`
   flex: 0 0 70%;
